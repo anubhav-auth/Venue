961eb42851687b1845c98aa88206c853748dbb27
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2026-03-04 19:17:46 +0530

    hq

---
+++ venue/src/main/java/com/anubhavauth/venue/dto/AllocationDto.java
@@ -0,0 +1,24 @@
+package com.anubhavauth.venue.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AllocationDto {
+    private Long assignmentId;
+    private Long studentId;
+    private String regNo;
+    private String name;
+    private String lastName;
+    private String degree;
+    private Integer passoutYear;
+    private String role;
+    private Long roomId;
+    private String roomName;
+    private String building;
+    private String floor;
+    private String seatNumber;   // null = overflow
+    private String day;
+    private boolean overflow;
+}
