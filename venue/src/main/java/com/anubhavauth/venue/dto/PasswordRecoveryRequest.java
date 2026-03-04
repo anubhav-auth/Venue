84851041590096ec5e0622cee3d704856aecb6ae
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2026-03-04 17:21:19 +0530

    hf

---
+++ venue/src/main/java/com/anubhavauth/venue/dto/PasswordRecoveryRequest.java
@@ -0,0 +1,14 @@
+package com.anubhavauth.venue.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class PasswordRecoveryRequest {
+
+    @NotBlank(message = "Registration number is required")
+    private String regNo;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+}
