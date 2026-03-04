1a9089ec9e59759b08e21d79b1b02319e6a56349
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2026-03-04 17:21:13 +0530

    hf

---
+++ venue/src/main/java/com/anubhavauth/venue/dto/LoginResponse.java
@@ -0,0 +1,34 @@
+package com.anubhavauth.venue.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+    private Long userId;
+    private String username;
+    private String role;
+    private String name;
+
+    // Only populated for VERIFIER role
+    private List<VerifierAssignmentDto> assignments;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class VerifierAssignmentDto {
+        private String day;
+        private Long roomId;
+        private String roomName;
+    }
+}
