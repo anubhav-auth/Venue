b8b8694753f84964772b54cfe9ab215fb0a98013
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2026-03-04 17:21:29 +0530

    hf

---
+++ venue/src/main/java/com/anubhavauth/venue/dto/RoomResponse.java
@@ -0,0 +1,20 @@
+package com.anubhavauth.venue.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class RoomResponse {
+
+    private Long id;
+    private String roomName;
+    private Integer capacity;
+    private Integer seatsPerRow;
+    private String building;
+    private String floor;
+    private String day;
+    private LocalDateTime createdAt;
+}
