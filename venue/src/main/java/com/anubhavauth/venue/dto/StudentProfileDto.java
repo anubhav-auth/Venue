e39d38c6e4b943cee6875abb6a2aaeaff1837672
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2026-03-04 17:45:04 +0530

    hf

---
+++ venue/src/main/java/com/anubhavauth/venue/dto/StudentProfileDto.java
@@ -0,0 +1,19 @@
+package com.anubhavauth.venue.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class StudentProfileDto {
+    private Long id;
+    private String regNo;
+    private String name;
+    private String lastName;
+    private String email;
+    private String degree;
+    private Integer passoutYear;
+    private String contactNo;
+    private String role;
+    private Boolean isPromoted;
+}
