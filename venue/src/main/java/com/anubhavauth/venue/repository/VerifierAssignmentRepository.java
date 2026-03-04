6d89868a1d18d0793a8cdfb91f07f9387d968b25
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2026-03-04 18:45:27 +0530

    hf

--- venue/src/main/java/com/anubhavauth/venue/repository/VerifierAssignmentRepository.java
+++ venue/src/main/java/com/anubhavauth/venue/repository/VerifierAssignmentRepository.java
@@ -2,42 +2,30 @@ package com.anubhavauth.venue.repository;
 
 import com.anubhavauth.venue.entity.VerifierAssignment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface VerifierAssignmentRepository extends JpaRepository<VerifierAssignment, Long> {
 
     Optional<VerifierAssignment> findByVerifierIdAndDay(Long verifierId, String day);
 
+    @Modifying
+    @Query("DELETE FROM VerifierAssignment va WHERE va.verifier.id = :verifierId AND va.day = :day")
+    void deleteByVerifierIdAndDay(@Param("verifierId") Long verifierId, @Param("day") String day);
+
     long countByVerifierId(Long verifierId);
 
-    void deleteByVerifierIdAndDay(Long verifierId, String day);
-
-    // Returns verifiers who have assignments ONLY on the given day (no other days)
     @Query("""
-        SELECT va FROM VerifierAssignment va
+        SELECT DISTINCT va.verifier.id FROM VerifierAssignment va
         WHERE va.verifier.id NOT IN (
             SELECT va2.verifier.id FROM VerifierAssignment va2
             WHERE va2.day != :day
         )
         AND va.day = :day
     """)
-    List<VerifierAssignment> findVerifiersWithOnlyDay(@Param("day") String day);
-
-    @Query("""
-    SELECT DISTINCT va.verifier.id FROM VerifierAssignment va
-    WHERE va.verifier.id NOT IN (
-        SELECT va2.verifier.id FROM VerifierAssignment va2
-        WHERE va2.day != :day
-    )
-    AND va.day = :day
-""")
     List<Long> findVerifierIdsWithOnlyDay(@Param("day") String day);
-
-
 }
